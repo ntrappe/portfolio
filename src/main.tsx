56ef4ddd54eee2f93cc8f9097a913a0e393653bc
Nicole <nien@Nicoles-Chonk.local>
2023-12-15 15:57:20 -0800

    frosted header and dynamic sizing done

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('body')!).render(
+  <React.StrictMode>
+    <>
+      <p>HI</p>
+    </>
+  </React.StrictMode>
+)
