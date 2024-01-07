4060d0e59481605f7fa130beea2df48cb4e00ab2
Nicole <nien@Nicoles-Chonk.local>
2024-01-07 00:12:02 -0800

    change from jsx to js file type

---
+++ src/Hello.js
@@ -0,0 +1,14 @@
+function Hello() {
+
+  return (
+    <>
+      <figure id="hello-background"></figure>
+      <div id="hello-text-container">
+        <h1 id="hello-text" family="romance">Hello</h1>
+        <span id="text-cursor"></span>
+      </div>
+    </>
+  )
+}
+
+export default Hello;
